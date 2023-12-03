4da4cd395e18d096952fc145f270f0128e78fbc2
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-03 15:02:55 -0400

    feat: add json middleware and configure routes in application

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import { errorHandler } from '../middlewares/error.middleware'
+import routes from '../routes'
 
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(routes)
 app.use(errorHandler)
 
 export default app
