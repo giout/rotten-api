c93b9a9540fa712eda86444051f405be4c74c126
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-03 21:07:05 -0400

    fix: add entities to schema and add foreign keys to ratings entity

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS rotten;
 
-CREATE TABLE IF NOT EXISTS media (
+CREATE TABLE IF NOT EXISTS rotten.media (
     film_id SERIAL,
     media_title TEXT NOT NULL,
     overview TEXT NOT NULL,
@@ -13,21 +13,21 @@ CREATE TABLE IF NOT EXISTS media (
     PRIMARY KEY(film_id)
 );
 
-CREATE TABLE IF NOT EXISTS genre (
+CREATE TABLE IF NOT EXISTS rotten.genre (
     genre_id SERIAL,
     genre_title TEXT NOT NULL,
     PRIMARY KEY (genre_id)
 );
 
-CREATE TABLE IF NOT EXISTS media_genre (
+CREATE TABLE IF NOT EXISTS rotten.media_genre (
     media_id INTEGER,
     genre_id INTEGER,
     PRIMARY KEY (media_id, genre_id),
-    FOREIGN KEY media_id REFERENCES media (media_id) ON DELETE CASCADE,
-    FOREIGN KEY genre_id REFERENCES genre (genre_id) ON DELETE CASCADE
+    FOREIGN KEY media_id REFERENCES rotten.media (media_id) ON DELETE CASCADE,
+    FOREIGN KEY genre_id REFERENCES rotten.genre (genre_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS rotten.users (
     user_id SERIAL,
     username TEXT NOT NULL,
     first_name TEXT NOT NULL,
@@ -38,31 +38,33 @@ CREATE TABLE IF NOT EXISTS users (
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE IF NOT EXISTS reviews (
+CREATE TABLE IF NOT EXISTS rotten.reviews (
     review_id SERIAL,
     user_id INTEGER NOT NULL,
     review_content TEXT NOT NULL,
     media_id INTEGER NOT NULL,
     review_date TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (review_id),
-    FOREIGN KEY media_id REFERENCES media (media_id) ON DELETE CASCADE,
-    FOREIGN KEY user_id REFERENCES users (user_id) ON DELETE CASCADE
+    FOREIGN KEY media_id REFERENCES rotten.media (media_id) ON DELETE CASCADE,
+    FOREIGN KEY user_id REFERENCES rotten.users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS rotten.comments (
     comment_id SERIAL,
     user_id INTEGER NOT NULL,
     comment_content TEXT NOT NULL,
     review_id INTEGER NOT NULL,
     comment_date TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (comment_id),
-    FOREIGN KEY review_id REFERENCES reviews (review_id) ON DELETE CASCADE,
-    FOREIGN KEY user_id REFERENCES users (user_id) ON DELETE CASCADE
+    FOREIGN KEY review_id REFERENCES rotten.reviews (review_id) ON DELETE CASCADE,
+    FOREIGN KEY user_id REFERENCES rotten.users (user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ratings (
+CREATE TABLE IF NOT EXISTS rotten.ratings (
     user_id INTEGER,
     media_id INTEGER,
     score NUMERIC NOT NULL,
-    PRIMARY KEY (user_id, media_id)
+    PRIMARY KEY (user_id, media_id),
+    FOREIGN KEY media_id REFERENCES rotten.media (media_id) ON DELETE CASCADE,
+    FOREIGN KEY user_id REFERENCES rotten.users (user_id) ON DELETE CASCADE
 );
