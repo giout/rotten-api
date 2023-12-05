b2ac7fd49cbc8456744d1445512a8fca596c6215
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-05 14:51:41 -0400

    feat: initialize controllers for movies and ratings

---
+++ src/controllers/movies.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express"
+
+export const getAllMovies = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
+
+export const getMovieById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
