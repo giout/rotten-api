b58e46d98057782e9df00b86947249e6612b36cc
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-12-05 14:56:27 -0400

    feat: initialize controllers for reviews and shows

---
+++ src/controllers/reviews.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from "express"
+
+export const getAllReviews = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
+
+export const postReview = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
+
+export const getReviewById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
+
+export const deleteReview = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
---
+++ src/controllers/shows.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express"
+
+export const getAllShows = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
+
+export const getShowById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+    } catch(e) {
+        next(e)
+    }
+}
